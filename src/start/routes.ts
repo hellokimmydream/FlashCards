5a3e4731c3681861f0ab1d14b857b4182357ba89
CamilleJulieRais <rais.camille@gmail.com>
2026-02-20 13:57:23 +0100

    feat(start routes)
    [5][Done][mise a jour]

--- src/start/routes.ts
+++ src/start/routes.ts
@@ -1,12 +1,13 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+import AuthController from '#controllers/auth_controller'
 
-router.on('/').render('pages/home')
+router.get('/', async ({ view }) => view.render('pages/home'))
+
+router.get('/register', [AuthController, 'showRegister']).use(middleware.guest())
+router.post('/register', [AuthController, 'register']).use(middleware.guest())
+
+router.get('/login', [AuthController, 'showLogin']).use(middleware.guest())
+router.post('/login', [AuthController, 'login']).use(middleware.guest())
+
+router.post('/logout', [AuthController, 'logout']).use(middleware.auth())
