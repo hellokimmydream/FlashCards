fd5ea695c9d30ed2578501f2b830a1d2e6e3957a
CamilleJulieRais <rais.camille@gmail.com>
2026-02-20 16:33:19 +0100

    feat(controlleur et routes pour la creation de deck et gestion)
    [55][Done][recherche faite sur la logique a suivre pour cree un decks]

--- src/start/routes.ts
+++ src/start/routes.ts
@@ -1,9 +1,11 @@
 import router from '@adonisjs/core/services/router'
 import { middleware } from '#start/kernel'
 import AuthController from '#controllers/auth_controller'
+import DecksController from '#controllers/decks_controller'
 
 router.get('/', async ({ view }) => view.render('pages/home'))
 
+// routes pour authentification
 router.get('/register', [AuthController, 'showRegister']).use(middleware.guest())
 router.post('/register', [AuthController, 'register']).use(middleware.guest())
 
@@ -11,3 +13,13 @@ router.get('/login', [AuthController, 'showLogin']).use(middleware.guest())
 router.post('/login', [AuthController, 'login']).use(middleware.guest())
 
 router.post('/logout', [AuthController, 'logout']).use(middleware.auth())
+
+// route pour decks
+router.get('/decks', [DecksController, 'index']).use(middleware.auth())
+router.get('/decks/create', [DecksController, 'create']).use(middleware.auth())
+router.post('/decks', [DecksController, 'store']).use(middleware.auth())
+
+router.get('/decks/:id/edit', [DecksController, 'edit']).use(middleware.auth())
+router.post('/decks/:id', [DecksController, 'update']).use(middleware.auth())
+
+router.post('/decks/:id/delete', [DecksController, 'destroy']).use(middleware.auth())
